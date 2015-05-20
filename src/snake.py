64d60d42bfd61c4246d8ac11d8768afe4a9c3eda
Duarte Pompeu <pnt12@live.com.pt>
2015-05-20 01:04:12 +0100

    refactoring done, turning should be next

--- src/snake.py
+++ src/snake.py
@@ -7,52 +7,64 @@ class Piece:
 		self.position = Vec2(x,y)
 		self.speed = Vec2(speed_x, speed_y)
 
+	def update(self):
+		self.position.add(self.speed)
+	
+	def getX(self):
+		return self.position.x
+
+	def getY(self):
+		return self.position.y
+
+	def getSpeedX(self):
+		return self.speed.x
+
+	def getSpeedY(self):
+		return self.speed.y
+
+	def setSpeed(self, x, y):
+		self.speed = Vec2(x,y)
 	
 
 	
 class Snake:
 	
 	def __init__(self, x=0, y=0, size=0):
-		self.position = Vec2(x,y)
-		self.speed = Vec2(0,0)
+		head = Piece(x,y)
 
-		self.body_pieces = list()
-		self.body_pieces.append(self.position)
-
-		for i in xrange(1, size+1):
-			self.body_pieces.append(Vec2(x-i, y))
-			
+		self.body = list()
+		self.body.append(head)
 		
+		for i in xrange(1, size+1):
+			self.body.append(Piece(x-i, y))
+			
 		self.turning_points = list()
 		
 
 	def turn(self, x,y):
-		self.speed.x = x
-		self.speed.y = y
-
-		self.turning_points.append(0)
+		for piece in self.body:
+			piece.setSpeed(x,y)
+			
+		#self.turning_points.append(-1)
 
 
 	def update(self):
-		body = self.body_pieces
-
-		head = body[0]
-		head.add(self.speed)
+		body = self.body
 		
-		for i in xrange(1, len(body)):
+		for i in xrange(0, len(body)):
 			piece = body[i]
-			piece.add(self.speed)
+			piece.update()
 
 	def size(self):
-		return len(self.body_pieces)
+		return len(self.body)
 
 
 	def draw(self):
 		head_symbol = self.get_head_symbol()
 		draw_cur(self.getX(), self.getY(), head_symbol)
 		
-		for piece in self.body_pieces[1:]:
-			draw_cur(piece.x, piece.y, "=")
+		for piece in self.body[1:]:
+			draw_cur(piece.getX(), piece.getY(), "=")
 
 
 	def get_head_symbol(self):
@@ -78,21 +90,28 @@ class Snake:
 
 	def toString(self):
 		msg = "SNAKE size: %d\n" % self.size()
-		msg += "pos: (%s) $ speed (%s)" %(self.position.toString(), self.speed.toString())
+		msg += "pos: (%s) $ speed (%s)" %(self.getPosition().toString(), self.getSpeed().toString())
 
 		return msg
 
 
 	def getX(self):
-		return self.position.x
-
+		return self.body[0].getX()
+		
 	def getY(self):
-		return self.position.y
+		return self.body[0].getY()
 
 	def getSpeedX(self):
-		return self.speed.x
+		return self.body[0].getSpeedX()
 
 	def getSpeedY(self):
-		return self.speed.y
+		return self.body[0].getSpeedY()
 
-		
+	def setSpeed(self, x, y):
+		self.body[0].setSpeed(x,y)
+
+	def getPosition(self):
+		return self.body[0].position
+
+	def getSpeed(self):
+		return self.body[0].speed
