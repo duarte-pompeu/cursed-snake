6e8fdc91e3adb25f2d7afdffb6b840b060184cf2
Your Name <you@example.com>
2021-08-03 22:53:45 +0100

    improve log messages regarding high score update

--- src/services/score_service.py
+++ src/services/score_service.py
@@ -11,17 +11,17 @@ class ScoreService:
         return self.repo.get_score()
     
     def enter_new_score(self, score: int):
-        logger.info(f"Score was {score}")
+        logger.info(f"Entered a new score: {score}")
 
         high_score = self.get_highscore()
         logger.info(f"Current high score is {high_score}")
 
         new_high_score = score > high_score
         if new_high_score:
-            logger.info(f"High score, will update high score. Congratz!")
+            logger.info(f"High score was beaten, will update it. Congratz!")
             self.repo.update_score(score)
         else:
-            logger.info("Lower score, will not update high score.")
+            logger.info("High score remains unbeaten, will not update it.")
 
         
 
